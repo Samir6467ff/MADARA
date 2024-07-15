fb4ec01280c19fd31bfd526e7691183c7e78e08f
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-07-15 14:43:54 +0300

    Update role.js

--- plugins/role.js
+++ plugins/role.js
@@ -26,7 +26,7 @@ while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 *╰━⊰ ${lenguajeGB.smsAutoLv1()} ⊱━━დ*
 
 *_${lenguajeGB.smsAutoLv6()}_*`, fkontak, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb }}}) */
-conn.sendButton(m.chat, '🐈 '`
+conn.sendButton(m.chat, '🐈 ' + wm,`
 *╭━⊰ ${username} ⊱━დ*
 *┃ ${lenguajeGB.smsAutoLv2()} ${before}*
 *┃ ${lenguajeGB.smsAutoLv3()} ${user.level}*
