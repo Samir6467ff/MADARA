0c463a104d0ff729a4db8794e9a49ef907d7b16c
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-07-15 03:46:54 +0300

    Update الباقات.js

--- plugins/الباقات.js
+++ plugins/الباقات.js
@@ -26,8 +26,10 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
   await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })
   const harley = 'https://telegra.ph/file/f97d2943120c84579f236.png'
+  const mentionId = m.key.participant || m.key.remoteJid;
  
-conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `harley`}, body: { text: `*مــرحــبــا بــك/ي يــا* ${taguser}
+conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `harley`}, body: { text: `
+*مــرحــبــا بــك/ي* @${mentionId.split('@')[0]}
 ╗──────¤
 > ⌈🚀╎سورس مادارا╎🚀⌋
 > •اختار احدي الباقات في الاسفل
