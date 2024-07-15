2ab818e391fb2a7ee181cc3e3b6f3c9cbcc1dce7
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-07-15 07:40:45 +0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -1,4 +1,4 @@
-let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
 if (!(isAdmin || isOwner)) {
 global.dfail('admin', m, conn)
 throw false
