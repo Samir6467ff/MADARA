2c6ef7f97c47332ce72d988654753cf6977bb745
HARLEY LIGHT <158737422+omar-lite@users.noreply.github.com>
2024-07-15 06:42:49 +0300

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,10 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `❐ رسالة : ${pesan}`
-let teks = `╭━〔 *⤹⚜ المنشن ⊰⚡⊱ الجماعي ⚜⤸* 〕━⬣\n*⤹⚜⊰تعالة يا بهيمة منك ليه 🐤⊱⚜⤸*\n${oi}\n`
+let teks = `⬣━〔 *⤹⚜ المنشن ⊰⚡⊱ الجماعي ⚜⤸* 〕╭━\n\n*⤹⚜⊰تعالة يا بهيمة منك ليه 🐤⊱⚜⤸*\n\n${oi}\n`
 for (let mem of participants) {
 teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `╰━━━━━[ *𝐇𝐀𝐑𝐋𝐄𝐘 𝐋𝐈𝐆𝐇𝐓* ]━━━━━⬣`
+teks += `⬣━━━━━[ *𝐇𝐀𝐑𝐋𝐄𝐘 𝐋𝐈𝐆𝐇𝐓* ]━━━━━╰`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|منشن|invocacion|todos|invocación)$/i
@@ -16,3 +16,4 @@ handler.admin = true
 handler.group = true
 handler.botAdmin = true
 export default handler
+
